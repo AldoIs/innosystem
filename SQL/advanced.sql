f96866824300450c9d4d7e1773e549aa786e4082
AldoIs <aldois.rov@gmail.com>
2015-12-13 03:53:30 -0800

    Agrego la base de datos comun en la carpeta common/SQL

---
+++ SQL/advanced.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-12-2015 a las 12:51:23
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `advanced`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `migration`
+--
+
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1448780369),
+('m130524_201442_init', 1448780376);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profile`
+--
+
+CREATE TABLE IF NOT EXISTS `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `correo` int(11) NOT NULL,
+  `cumpleanos` date NOT NULL,
+  `telefono` text NOT NULL,
+  `invocador` text NOT NULL,
+  `facebook` text NOT NULL,
+  `twitter` text NOT NULL,
+  `twitch` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  `role` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `role`) VALUES
+(1, 'admin', 'n-v6jIyknERgEizFoa0P7-ryq7Z0db9T', '$2y$13$gD5qroNiAHDheUCTvVFRmebXObXJdvxeCQrkEI0GjuPvT.jOC8MN.', 'Bw6juAJ5Zf5F00uadxk-nQh_Sz3_D7F7_1449992187', 'aldois.rov@gmail.com', 10, 1448780717, 1449992187, 0),
+(2, 'zapato', 'Sfm6oNvBEjbrsL5puVRbD-vHhbeqkghw', '$2y$13$FPMUyueqkRxGrI7He2mZx.bvkr6FkWcAcHiZr9NsYCrBF6nomTkDa', NULL, 'aldo@a.com', 10, 1449992213, 1449992213, 0);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
