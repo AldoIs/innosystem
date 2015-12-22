1d78ab1236653759754e4b2326dbd16415ceaa38
AldoIs <aldois.rov@gmail.com>
2015-12-21 22:28:33 -0800

    Actualizacion de main.php configuracion remota

--- common/config/main.php
+++ common/config/main.php
@@ -5,5 +5,16 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=advanced',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
     ],
 ];
