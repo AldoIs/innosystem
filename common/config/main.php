b365a49774644b4b6934a599c8114165114f11bf
AldoIs <aldois.rov@gmail.com>
2015-12-23 10:50:25 -0800

    Se agrego una base  de datos provicional de godaddy

--- common/config/main.php
+++ common/config/main.php
@@ -7,9 +7,9 @@ return [
         ],
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=advanced',
-            'username' => 'root',
-            'password' => 'root',
+            'dsn' => 'mysql:host=wsiemarketingmexico.com;dbname=aldoDB',
+            'username' => 'aldois',
+            'password' => '123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
