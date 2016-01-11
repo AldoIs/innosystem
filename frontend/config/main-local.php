18d08f275d64b735a045f844699b92560cde5174
AldoIs <aldois.rov@gmail.com>
2016-01-10 19:13:40 -0800

    Fixed tracking ignore

--- frontend/config/main-local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'OKg3W3F4MoHKm1dODSAvUprRiuieltrW',
-        ],
-    ],
-];
